8aeca7d1fa73af356d4e53503ac58468dbeebac1
wangzhijun <wzhj0528@163.com>
2018-11-12 15:35:56 +0800

    add butterKnife, add kapt plugin

--- app/src/main/java/com/annarm/douban/moive/MainActivity.kt
+++ app/src/main/java/com/annarm/douban/moive/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.annarm.douban.moive
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.annarm.douban.moive.app.DMApplication
+import com.annarm.douban.moive.home.HomeActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         Toast.makeText(DMApplication.instance, "kot lin lin a", Toast.LENGTH_SHORT).show()
+        startActivity(Intent(this, HomeActivity::class.java))
     }
 }
--- app/src/main/java/com/annarm/douban/moive/home/HomeActivity.kt
+++ app/src/main/java/com/annarm/douban/moive/home/HomeActivity.kt
@@ -1,5 +1,14 @@
 package com.annarm.douban.moive.home
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
+import butterknife.Unbinder
+import com.annarm.douban.moive.R
 import com.annarm.douban.moive.base.BaseActivity
 
 /**
@@ -9,5 +18,27 @@ import com.annarm.douban.moive.base.BaseActivity
  * @version V1.0
  */
 class HomeActivity : BaseActivity(){
+    var unBinder:Unbinder? = null
+
+    @BindView(R.id.tv) lateinit var tv:TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        unBinder = ButterKnife.bind(this)
+        tv.text = "learn Kotlin"
+    }
+
+    @OnClick(R.id.tv)
+    fun onClick(view: View){
+        when (view.id){
+            R.id.tv -> Toast.makeText(this, "lla", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unBinder!!.unbind()
+    }
 
 }
